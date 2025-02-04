ab1338fb3c29148d37a2e87c9ff5fad2d84f9615
chahdhattab <eechahdee74@gmail.com>
2025-02-04 14:55:27 +0100

    manifest/init

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "gestion_notes",
+    'version': '1.0',
+    'author': "chahd Leila Sanae Chaymae",
+    'category': 'Category',
+    'description': "Description de mon module",
+    'depends': ['base'],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+}
