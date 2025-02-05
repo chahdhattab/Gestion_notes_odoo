f023b14295ee6132697b5870c896ac6547b59468
leilajaafar <leilajaafar7@gmail.com>
2025-02-05 15:39:21 +0100

    edit 'data' files in manifest.py

--- __manifest__.py
+++ __manifest__.py
@@ -6,6 +6,10 @@
     'description': "Description de mon module",
     'depends': ['base'],
     'data': [
+        'security/gestion_notes_security.xml',
+        'security/ir.model.access.csv',
+        'views/gestion_notes_menu.xml',
+        'views/gestion_notes_views.xml',
     ],
     'installable': True,
     'application': True,
